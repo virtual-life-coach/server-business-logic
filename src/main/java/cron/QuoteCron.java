065e1fd79405cc6ec567e9c81ade5959b8a5534d
ShadowTemplate <taneburgo+shadowtemplate@gmail.com>
2017-01-27 12:30:47 +0100

    - Fixed GAE configurations; - Cron job example

---
+++ src/main/java/cron/QuoteCron.java
@@ -0,0 +1,18 @@
+package cron;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.logging.Logger;
+
+public class QuoteCron extends HttpServlet {
+
+    private static final Logger log = Logger.getLogger(QuoteCron.class.getName());
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        log.info("Received GET request");
+    }
+}
