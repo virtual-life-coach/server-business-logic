bbd1030f59d1ae9c79947848596d63187fee174f
ShadowTemplate <taneburgo+shadowtemplate@gmail.com>
2017-01-28 18:35:52 +0100

    - Micro-fixes

--- src/main/java/vlc/bl/dispatcher/RequestDispatcher.java
+++ src/main/java/vlc/bl/dispatcher/RequestDispatcher.java
@@ -2,15 +2,12 @@ package vlc.bl.dispatcher;
 
 import vlc.common.connectors.Telegram;
 import vlc.common.to.QuoteTO;
-import vlc.common.to.TelegramMessageTO;
 import vlc.common.util.Constants;
 import vlc.ldb.soap.LocalDatabase;
 import vlc.ldb.soap.LocalDatabaseService;
 import vlc.ldb.soap.UserTO;
 
 import javax.ws.rs.client.ClientBuilder;
-import javax.ws.rs.client.Entity;
-import javax.ws.rs.client.Invocation;
 import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
@@ -22,7 +19,7 @@ public class RequestDispatcher {
         LocalDatabase service = new LocalDatabaseService().getLocalDatabaseImplPort();
         List<UserTO> users = service.listUsers();
         for (UserTO user : users) {
-            String randomQuote = Constants.EXTERNAL_ADAPTER_ENDPOINT;
+            String randomQuote = getRandomQuote();
             Telegram.sendMessage(user.getTelegramId(), randomQuote);
         }
     }
@@ -32,6 +29,6 @@ public class RequestDispatcher {
         Response response = resource.request().accept(MediaType.APPLICATION_JSON).get();
         response.bufferEntity();
         QuoteTO quoteTO = response.readEntity(QuoteTO.class);
-        return quoteTO.getQuote()
+        return quoteTO.getQuote();
     }
 }
